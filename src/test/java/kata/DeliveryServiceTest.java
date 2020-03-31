117dc578ea8a656adb3a6008c3dba426af6b3eaa
Andrea Francia <andrea@andreafrancia.it>
2020-03-31 21:38:56 +0200

    Refactor.

--- src/test/java/kata/DeliveryServiceTest.java
+++ src/test/java/kata/DeliveryServiceTest.java
@@ -7,10 +7,12 @@ import java.util.List;
 import static java.util.Collections.emptyList;
 import static org.junit.Assert.assertEquals;
 
-public class FirstTest {
+public class DeliveryServiceTest {
+
+    private final DeliveryService service = new DeliveryService();
+
     @Test
-    public void something() {
-        var service = new DeliveryService();
+    public void noCollectionPoints() {
 
         List<String> result = service.collectionPointsForPackageOfSize(10);
 
