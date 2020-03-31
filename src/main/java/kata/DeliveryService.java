cd418fdb20c20d59fb9be0249c6bf2642adee696
Andrea Francia <andrea@andreafrancia.it>
2020-03-31 21:44:27 +0200

    Refactor.

--- src/main/java/kata/DeliveryService.java
+++ src/main/java/kata/DeliveryService.java
@@ -7,15 +7,16 @@ public class DeliveryService {
     private List<String> lockers = new ArrayList<>();
     private int box;
 
-    public List<String> collectionPointsForPackageOfSize(int i) {
-        var result = new ArrayList<String>();
-        if(box >= i)
-            result.addAll(lockers);
-        return result;
-    }
-
     public void addLocker(String name, int box) {
         this.box = box;
         lockers.add(name);
     }
+
+    public List<String> collectionPointsForPackageOfSize(int packageSize) {
+        var result = new ArrayList<String>();
+        if(box >= packageSize)
+            result.addAll(lockers);
+        return result;
+    }
+
 }
